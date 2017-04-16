2ebdf0c91b233f9693982622d1fc9902dd9f36e8
worthingtont12 <worthingtont12@gmail.com>
2017-04-16 17:55:36 -0400

    palceholders for stephen

--- Random_Forest/Random_Forest_EC2.py
+++ Random_Forest/Random_Forest_EC2.py
@@ -5,22 +5,21 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 import sklearn.feature_extraction.text as text
 from sklearn import model_selection
-from boto.s3.connection import S3Connection
 
 # log updates
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-# import data
-# connect to s3
-conn = boto.connect_s3()
+# import one day
+df = pd.read_csv("~/test.csv")
 
-# connect to gdelt
-gdelt = conn.get_bucket('gdelt-open-data')
-gdelt.list()
+# create new binary variable
+df["negative_impact"] =
+
+# drop unneeded variables
 
 # splitting target attribute from examples
-X = tfidfs[tfidfs.columns[:-1]]
-Y = np.array(tfidfs.loc[:, ['random']])
+X = df[df.columns[:-1]]
+Y = np.array(df.loc[:, ['negative_impact']])
 
 # Random Forest Model
 seed = 7
