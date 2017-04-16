b17c720a77a8504ded74a5ebde00f7d99dc99071
worthingtont12 <worthingtont12@gmail.com>
2017-04-16 16:38:45 -0400

    first commit'

---
+++ Random_Forest_EC2.py
@@ -0,0 +1,28 @@
+"""Parallelized Random Forest Code for Benchmarking EC2
+"""
+import logging
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+import sklearn.feature_extraction.text as text
+from sklearn import model_selection
+
+# log updates
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+# import data
+
+# splitting target attribute from examples
+X = tfidfs[tfidfs.columns[:-1]]
+Y = np.array(tfidfs.loc[:, ['random']])
+
+# Random Forest Model
+seed = 7
+rf = RandomForestClassifier(n_jobs=-1)
+
+# Cross Validate
+kfold = model_selection.KFold(n_splits=10, random_state=seed)
+results = model_selection.cross_val_score(rf, X, Y.ravel(), cv=kfold)
+
+# results
+print(results.mean())
