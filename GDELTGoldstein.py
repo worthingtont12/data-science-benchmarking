dc22d1af3a267bfaa4edc070512824774eda0fc3
Matt <md3es@virginia.edu>
2017-04-20 08:19:09 -0400

    goldstein value fix

--- GDELTGoldstein.py
+++ GDELTGoldstein.py
@@ -10,6 +10,20 @@ df = df[np.isfinite(df[30])]
 upper = np.percentile(df.ix[:,30],75)
 lower = np.percentile(df.ix[:,30],25)
 
+print(df[1:5][30])
+
+def check(x, high, low):
+    if x > high:
+        return 1
+    elif x < low:
+        return 0
+    else:
+        return -1
+
+df[30] = df[30].apply(lambda x: check(x, upper,lower))
+
+print(df[1:5][30])
+
 # I was trying to write a loop at this point to assign values(1,0) to the upper 25% and
 # lower 25% and then drop all the other values. For some reason it wasn't reassigning
 # all the values that I was hoping it would and the runtime was pretty long just for one
