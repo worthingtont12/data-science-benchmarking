634a62504f7ec4586911b610c6c5b301665bd3b0
Stephen Morris <stephenmorris@Stephens-MacBook-Pro.local>
2017-04-19 22:57:33 -0400

    adding sample csv and python code

---
+++ GDELTGoldstein.py
@@ -0,0 +1,19 @@
+
+# coding: utf-8
+
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('sample.csv', delimiter= '\t', header = None)
+df = df.drop(df.columns[[0,1,2,3,4,6,16,36,38,41,43,45,48,50,52,55,56,57]], axis=1)
+df = df[np.isfinite(df[30])]
+upper = np.percentile(df.ix[:,30],75)
+lower = np.percentile(df.ix[:,30],25)
+
+# I was trying to write a loop at this point to assign values(1,0) to the upper 25% and
+# lower 25% and then drop all the other values. For some reason it wasn't reassigning
+# all the values that I was hoping it would and the runtime was pretty long just for one
+# day. I'm not familiar enough with Python to know if there is an apply type function
+# that would be faster than a loop. Nonetheless, this removes all the unnecessary variables
+# and the Goldstein Scale is column 30. I think we should just deal with the highest and
+# lowest values and drop all the neutral results. 
