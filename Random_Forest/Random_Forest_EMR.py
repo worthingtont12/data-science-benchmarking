6e74b04de5488b13f9f7bd4784a93e583007b9c6
worthingtont12 <worthingtont12@gmail.com>
2017-04-20 13:33:27 -0400

    testing at scale

--- Random_Forest/Random_Forest_EMR.py
+++ Random_Forest/Random_Forest_EMR.py
@@ -1,8 +1,11 @@
 """Random Forest Code for Benchmarking EMR."""
+import timeit
+import logging
 from pyspark.mllib.tree import RandomForest, RandomForestModel
 from pyspark.mllib.util import MLUtils
 from pyspark import SparkContext
 sc = SparkContext()
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
 # Load and parse the data file into an RDD of LabeledPoint.
 data = MLUtils.loadLibSVMFile(sc, 'sample_libsvm_data.txt')
@@ -13,10 +16,11 @@ data = MLUtils.loadLibSVMFile(sc, 'sample_libsvm_data.txt')
 #  Empty categoricalFeaturesInfo indicates all features are continuous.
 #  Note: Use larger numTrees in practice.
 #  Setting featureSubsetStrategy="auto" lets the algorithm choose.
-
+start_time = timeit.default_timer()
 model = RandomForest.trainClassifier(trainingData, numClasses=2, categoricalFeaturesInfo={},
                                      numTrees=3, featureSubsetStrategy="auto",
                                      impurity='gini', maxDepth=4, maxBins=32, seed=7)
+print('Time to train model: ' + str(timeit.default_timer() - start_time) + ' seconds')
 
 # Evaluate model on test instances and compute test error
 predictions = model.predict(testData.map(lambda x: x.features))
@@ -26,7 +30,7 @@ testErr = labelsAndPredictions.filter(
 print('Test Error = ' + str(testErr))
 print('Learned classification forest model:')
 print(model.toDebugString())
-
-# Save and load model
-model.save(sc, "target/tmp/myRandomForestClassificationModel")
-sameModel = RandomForestModel.load(sc, "target/tmp/myRandomForestClassificationModel")
+#
+# # Save and load model
+# model.save(sc, "target/tmp/myRandomForestClassificationModel")
+# sameModel = RandomForestModel.load(sc, "target/tmp/myRandomForestClassificationModel")
